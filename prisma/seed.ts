8af035969609adc2d9f9b4f9d66072674e8c63c8
gerysantoso03 <gerysantos03@gmail.com>
2025-06-18 11:35:09 +0700

    Remove user seeder for prisma.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from '@/generated/prisma'
-import { seedUsers } from './seeders/users'
 import { seedCategories } from './seeders/categories'
 
 const prisma = new PrismaClient();
 
 async function main() {
     console.log('Running...');
-    await seedUsers();
     await seedCategories();
     console.log('✅ All data seeded');
 }
