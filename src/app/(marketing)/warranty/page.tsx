484b6da88ce40a733a247c29e6459491485a7e63
gerysantoso03 <gerysantos03@gmail.com>
2025-06-25 00:36:19 +0700

    Warranty page responsive.

--- src/app/(marketing)/warranty/page.tsx
+++ src/app/(marketing)/warranty/page.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/shared/components/banner/Banner';
 import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -34,15 +35,10 @@ const WarrantyPage = () => {
     <main className="warranty">
       {/* Warranty Banner */}
       <section className="section-wrapper" id="warrantyBanner">
-        <div className="relative h-[58rem]">
-          <Image
-            src="/image/warranty/Photo 1_BASF x Panasonic.JPG"
-            alt="BASF x Panasonic"
-            fill
-            className="border-img w-full rounded-[1.6rem] object-cover object-[0_-80px]"
-          />
-          <div className="absolute bottom-[4rem] left-[4rem]">
-            <h1 className="font-bold leading-[8rem] text-[8rem] w-[70%]">
+        <div className="relative">
+          <Banner image={''} bannerFor="warranty" />
+          <div className="absolute bottom-[4rem] left-[2rem] md:left-[4rem] z-[2]">
+            <h1 className="font-bold leading-[100%] md:leading-[8rem] text-[4rem] md:text-[8rem] md:w-[70%]">
               Deskripsi Layanan Garansi Produk
             </h1>
           </div>
@@ -52,9 +48,9 @@ const WarrantyPage = () => {
       {/* Warranty Info */}
       <section
         id="warrantyInfo"
-        className="section-wrapper h-[68rem] flex items-center justify-center"
+        className="section-wrapper md:h-[68rem] flex items-center justify-center"
       >
-        <p className="text-center text-[4rem] font-normal tracking-[0.02rem] px-[4rem]">
+        <p className="text-center text-[3rem] md:text-[4rem] font-normal tracking-[0.02rem] md:px-[4rem] py-[6rem] md:py-0">
           Sebagai merek dari BASF, perusahaan kimia terbesar di dunia yang
           berdiri sejak tahun 1865, RODIM PPF berdiri di atas fondasi keunggulan
           global dan komitmen jangka panjang. Solusi perlindungan kami dibentuk
@@ -64,7 +60,7 @@ const WarrantyPage = () => {
 
       {/* Warranty Products */}
       <section className="section-wrapper" id="warrantyProducts">
-        <div className="bg-[var(--secondary-black)] rounded-[1.8rem] p-[12rem]">
+        <div className="bg-[var(--secondary-black)] rounded-[1.8rem] p-[4rem_2rem] md:p-[12rem]">
           {/* Warranty Products Title */}
           <p className="font-normal text-[2.4rem] leading-[2.8rem] mb-20">
             Kami memastikan tidak akan ada retakan, gelembung, residu lem yang
@@ -73,12 +69,12 @@ const WarrantyPage = () => {
             produk PPF, kami menawarkan penggantian gratis selama periode
             garansi. Setiap produk dijamin dengan garansi yang dapat dipercaya.
           </p>
-          <h1 className="font-bold text-[4.6rem] leading-[100%] mb-20">
+          <h1 className="font-bold text-[4.6rem] md:leading-[100%] mb-20">
             Pastikan anda mendapatkan garansi dari perusahaan kelas dunia
           </h1>
 
           {/* Warranty Products Group 1 */}
-          <div className="grid grid-cols-3 gap-4 mb-5">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
             {warrantyProductsGroup1.map((card, idx) => (
               <div
                 className="rounded-[1.8rem] bg-black/40 py-[2.2rem] px-[3rem] flex flex-col justify-start"
@@ -101,7 +97,7 @@ const WarrantyPage = () => {
           </div>
 
           {/* Warranty Products Group 2 */}
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {warrantyProductsGroup2.map((card, idx) => (
               <div
                 className="rounded-[1.8rem] bg-black/40 py-[2.2rem] px-[3rem] flex flex-col justify-start"
