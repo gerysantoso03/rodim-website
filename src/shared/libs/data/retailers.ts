38897d338a6c05deb6b1fc7bf4246b0af218121b
Fadla Ichsan <fadla.ichsan@weekendinc.com>
2025-06-30 17:54:54 +0700

    Section(footer): tidy up footer.

---
+++ src/shared/libs/data/retailers.ts
@@ -0,0 +1,46 @@
+import { StaticImageData } from 'next/image';
+import IconTiktok from '../../../assets/icons/tiktok.svg';
+import IconInstagram from '../../../assets/icons/instagram.svg';
+import IconWhatsapp from '../../../assets/icons/whatsapp.svg';
+import IconTokopedia from '../../../assets/icons/tokopedia.png';
+import IconShopee from '../../../assets/icons/shopee.png';
+import IconBliBli from '../../../assets/icons/bli-bli.png';
+
+export type Retailers = {
+  image: StaticImageData | string;
+  title: string;
+  url: string;
+};
+
+export const retailers: Retailers[] = [
+  {
+    image: IconTiktok,
+    title: 'Tiktok',
+    url: '#'
+  },
+    {
+    image: IconInstagram,
+    title: 'Instagram',
+    url: '#'
+  },
+  {
+    image: IconWhatsapp,
+    title: 'Whatsapp',
+    url: '#'
+  },
+  {
+    image: IconTokopedia,
+    title: 'Tokopedia',
+    url: '#'
+  },
+  {
+    image: IconShopee,
+    title: 'Shopee',
+    url: '#'
+  },
+  {
+    image: IconBliBli,
+    title: 'Blibli',
+    url: '#'
+  }
+];
