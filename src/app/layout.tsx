925af2d2c1f8e17b9b93ad48e9ddbc8840128e2e
Fadla Ichsan <fadla.ichsan@weekendinc.com>
2025-07-25 11:08:14 +0700

    Initial configure SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,65 @@
 import './globals.css';
 
 export const metadata = {
-  metadataBase: "https://basfrodim.id/",
+  metadataBase: 'https://basfrodim.id/',
   title: {
-    template: "%s | RODIM",
-    default:
-      "RODIM", 
+    template: '%s | RODIM',
+    default: 'RODIM',
   },
   alternates: {
     canonical: '/',
   },
-  description: "RODIM",
-  keywords: ["RODIM", "BASF", "Paint Protection Film", "PPF", "PPF RODIM", "TPU film", "automotive paint protection", "car paint protection", "cat mobil", "pelindung cat mobil", "OEM paint protection", "TPU material", "auto detailing", "perlindungan cat mobil", "car protection film", "PPF berkualitas tinggi", "inovasi PPF", "RODIM Indonesia", "pelindung cat dari BASF"],
+  description: 'RODIM',
+  keywords: [
+    'RODIM',
+    'BASF',
+    'Paint Protection Film',
+    'PPF',
+    'PPF RODIM',
+    'TPU film',
+    'automotive paint protection',
+    'car paint protection',
+    'cat mobil',
+    'pelindung cat mobil',
+    'OEM paint protection',
+    'TPU material',
+    'auto detailing',
+    'perlindungan cat mobil',
+    'car protection film',
+    'PPF berkualitas tinggi',
+    'inovasi PPF',
+    'RODIM Indonesia',
+    'pelindung cat dari BASF',
+  ],
   openGraph: {
     title: "RODIM - Protection by World's Best",
-    description: "RODIM PPF is a brand of BASF, a global leader in TPU materials, automotive OEM and refinish paint. From TPU production to advanced PPF innovation, RODIM delivers complete solutions and truly understands how to protect factory car paint.",
-    url: "https://basfrodim.id/",
+    description:
+      'RODIM PPF is a brand of BASF, a global leader in TPU materials, automotive OEM and refinish paint. From TPU production to advanced PPF innovation, RODIM delivers complete solutions and truly understands how to protect factory car paint.',
+    url: 'https://basfrodim.id/',
     siteName: "RODIM - Protection by World's Best",
     images: [
       {
-        url: "/image/rodim-og.jpg",
+        url: '/image/rodim-og.jpg',
         width: 600,
         height: 600,
       },
     ],
-    type: "website",
+    type: 'website',
   },
-  icons: [{
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '144x144',
-    url: '/image/rodim-icon.png',
-  }],
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '144x144',
+      url: '/image/rodim-icon.png',
+    },
+  ],
   twitter: {
-    card: "summary_large_image",
+    card: 'summary_large_image',
     title: "RODIM - Protection by World's Best",
-    description: "RODIM PPF is a brand of BASF, a global leader in TPU materials, automotive OEM and refinish paint. From TPU production to advanced PPF innovation, RODIM delivers complete solutions and truly understands how to protect factory car paint.",
-    images: ["/image/rodim-og.jpg"],
+    description:
+      'RODIM PPF is a brand of BASF, a global leader in TPU materials, automotive OEM and refinish paint. From TPU production to advanced PPF innovation, RODIM delivers complete solutions and truly understands how to protect factory car paint.',
+    images: ['/image/rodim-og.jpg'],
   },
 };
 
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,26 +1,24 @@
 import { getAllProductsAction } from '@/features/admin/actions/product/action';
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
-export const revalidate = 3600
+export const revalidate = 3600;
 type ProductProps = {
   id: number;
   code: string;
   slug: string | null;
   warranty?: string | null;
 };
- 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const data = await getAllProductsAction() as ProductProps[];
 
-  const products = data.map((item) => (
-    {
-      url: `https://basfrodim.id/product-rodim/${item.slug}`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
-      priority: 0.8,
-    }
-  ))
-  
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const data = (await getAllProductsAction()) as ProductProps[];
+
+  const products = data.map((item) => ({
+    url: `https://basfrodim.id/product-rodim/${item.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: 0.8,
+  }));
+
   return [
     {
       url: 'https://basfrodim.id/',
@@ -58,6 +56,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 0.5,
     },
-    ...products
-  ]
+    ...products,
+  ];
 }
