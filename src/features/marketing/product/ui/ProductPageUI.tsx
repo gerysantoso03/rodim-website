ada6946aaa2a228559e2f121961080d63a52f271
Susanto-Sqiva <susanto7768@sqiva.com>
2025-09-11 21:59:39 +0700

    Fixing image url product.

--- src/features/marketing/product/ui/ProductPageUI.tsx
+++ src/features/marketing/product/ui/ProductPageUI.tsx
@@ -45,8 +45,9 @@ const ProductPageUI = ({ data }: ProductPageUIProps) => {
                 : `/${imgSrc}`
           }
           alt="hero image"
-          width={1394}
-          height={800}
+          // width={1394}
+          // height={800}
+          fill
           className={`w-full h-full absolute z-[1] object-cover ${data.code.toUpperCase() == 'RODIM R2 MATTE' ? 'transform scale-[101%]' : ''}`}
           priority
         />
